f0675a413f6f4d450c6441c9546a623319e89bd0
Michael Stiles <mstiles01@icloud.com>
2020-05-22 11:39:31 -0500

    rendering other data types

--- cfehome/views.py
+++ cfehome/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
 
 def home_page(request):
     my_title = "Hello There..."
@@ -12,3 +13,10 @@ def about_page(request):
 
 def contact_page(request):
     return render(request, "hello_world.html", {"title": "Contact Info" })
+
+def example_page(request):
+    context = {"title": "Example"}
+    template_name = "hello_world.html"
+    template_obj = get_template(template_name)
+    render_item = template_obj.render(context)
+    return HttpResponse( render_item)

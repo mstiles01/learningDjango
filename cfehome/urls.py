f8c840e0fb69c204b92dcdb6adc9ed32e634206b
Michael Stiles <mstiles01@icloud.com>
2020-05-26 22:38:04 -0500

    5-22

--- cfehome/urls.py
+++ cfehome/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import (
+    blog_post_detail.html)
 from .views import (home_page,
     about_page,
     contact_page
 )
 urlpatterns = [
     path('', home_page),
+     path('blog', detail_page)
     path('about/', about_page),
     path('contact/', contact_page),
     path('admin/', admin.site.urls),

3f6bd177d86e990f6ad09d1e75aa21682e9f6a1a
Michael Stiles <mstiles01@icloud.com>
2020-05-27 22:57:28 -0500

    dynamic id url

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,8 @@ from .models import BlogPost
 
 
 
-def blog_post_detail_page(request):
-    obj = BlogPost.objects.get(id=1)
+def blog_post_detail_page(request, id):
+    obj = BlogPost.objects.get(id)
     template_name = 'blog_post_detail.html'
     context = {"object": obj}
     return render(request, "", {})

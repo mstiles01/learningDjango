c0028e8f9b054f6ff61348f091f9e689e4d28729
Michael Stiles <mstiles01@icloud.com>
2020-06-01 22:59:35 -0500

    simplifing 404

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.http import render, get_object_or_404
 
 # Create your views here.
 from .models import BlogPost
@@ -7,12 +7,8 @@ from .models import BlogPost
 
 
 def blog_post_detail_page(request, id):
-    try:
-        obj = BlogPost.objects.get(id=str(id))
-    except BlogPost.DoesNotExist:
-        raise Http404
-    except ValueError:
-        raise Http404
+    obj = get_object_or_404(BlogPost, id=id)
+    #obj = BlogPost.objects.get(id=str(id))
     template_name = 'blog_post_detail.html'
     context = {"object": obj}
     return render(request, "", {})
